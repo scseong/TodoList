299e588341b4a37b27adb7327df930472f7b97f1
scseong <goidtv@naver.com>
2023-03-27 23:19:09 +0900

    feat: 현재 위치한 url에 따라 NavLink css 적용 (#22)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 import Sidebar from '../components/Sidebar';
 import Board from '../components/Board';
 import { useToDosState } from '../reducer/ToDosContext';
+import { useLocation } from 'react-router';
+
+const filters = ['전체', '진행중', '완료'];
 
 export default function Home() {
+  const pathname = useLocation().pathname;
   const toDos = useToDosState();
   const toDosKeys = Object.keys(toDos);
-  const lengthToDos = Object.assign(
-    {},
-    ...toDosKeys.map((key) => ({ [key]: toDos[key].length })),
-  );
+  const toDosInfo = Object.assign([
+    ...toDosKeys.map((key) => ({
+      name: key,
+      path: `/${key === toDosKeys[0] ? '' : key}`,
+      length: toDos[key].length,
+    })),
+  ]);
 
   return (
     <>
-      <Sidebar lengthToDos={lengthToDos} />
+      <Sidebar toDosInfo={toDosInfo} pathname={pathname} />
       <Board />
     </>
   );
